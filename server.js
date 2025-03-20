f88e21fd5af52540f18f7fb2ba25fced1cf639d9
LordCartel007 <cartellord77@gmail.com>
2025-03-20 22:49:46 +0100

    updated server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 //   socket.emit("chat-message", "Hello World");
 // });
 
+// getting the socket.io library
 const io = require("socket.io")(3000, {
   cors: {
     origin: "http://127.0.0.1:5500",
